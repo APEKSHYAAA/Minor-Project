82d98324dba114a0d350836318d56d3cdfa0a23a
Virus <bishaltwr@gmail.com>
2023-12-22 17:25:00 +0545

    UI changes in version 2(chat-output toggle)

---
+++ try2/server.js
@@ -0,0 +1,36 @@
+
+
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+
+const PORT = 6969;
+
+http.createServer(function (request, response) {
+    var filePath = '.' + request.url;
+    if (filePath == './') {
+        filePath = './index.html';
+    }
+
+    var extname = String(path.extname(filePath)).toLowerCase();
+    var mimeTypes = {
+        '.html': 'text/html',
+        '.js': 'text/javascript',
+        '.css': 'text/css',
+        '.png': 'image/png',
+        // add more MIME types if needed
+    };
+
+    var contentType = mimeTypes[extname] || 'application/octet-stream';
+
+    fs.readFile(filePath, function(error, content) {
+        if (error) {
+            console.log(error);
+        } else {
+            response.writeHead(200, { 'Content-Type': contentType });
+            response.end(content, 'utf-8');
+        }
+    });
+
+}).listen(PORT);
+console.log(`Server running at http://localhost:${PORT}/`);
