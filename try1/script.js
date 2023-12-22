7e38b4f72daf37aee18e2c6c284dd8ade9416cb1
Virus <bishaltwr@gmail.com>
2023-12-22 15:22:18 +0545

    Final of version 1: lacks chat, output and collaboration

--- try1/script.js
+++ try1/script.js
@@ -11,6 +11,8 @@ form.addEventListener('submit', function(e){
         // '<li></li>'
         )
     message.value = "";
+    var chatHistory = document.querySelector('.chat-history');
+    chatHistory.scrollTop = chatHistory.scrollHeight;
 })
 
 window.onload = function() {
@@ -20,4 +22,35 @@ window.onload = function() {
     viewportMargin: Infinity,
   });
   editor.setSize(null, "100%");
+};
+
+let user_name = document.getElementById("username");
+
+// Save the value of the input box to localStorage whenever it changes
+username.addEventListener('input', function() {
+    localStorage.setItem('username', user_name.value);
+});
+
+
+
+
+
+
+
+// Set the value of the input box from localStorage when the page loads
+window.onload = function() {
+    var editor = CodeMirror(document.getElementById("code_with_highlight"), {
+        lineNumbers: true,
+        mode:  "javascript",
+        viewportMargin: Infinity,
+    });
+    editor.setSize(null, "100%");
+
+    // Get the saved value from localStorage
+    var savedUsername = localStorage.getItem('username');
+
+    // If there is a saved value, set the value of the input box to it
+    if (savedUsername !== null) {
+        username.value = savedUsername;
+    }
 };
--- try1/server.js
+++ try1/server.js
@@ -1,20 +1,4 @@
-// var http = require('http')
-// var fs = require('fs')
 
-// const PORT = 8080;
-
-// fs.readFile('./index.html',function(error,html) {
-//     if(error) {
-//         console.log(error);
-//     } else {
-//         console.log("http://localhost:${8080}")
-//         http.createServer(function(request, response) {
-//             response.writeHeader(200, {"Content-Type": "text/html"});
-//             response.write(html);
-//             response.end();
-//         }).listen(PORT)
-//     }
-// });
 
 var http = require('http');
 var fs = require('fs');
